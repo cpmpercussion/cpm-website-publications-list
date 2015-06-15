589f29a458f5fade765fbe905af15e5a228222aa
cpmpercussion <cpm@charlesmartin.com.au>
2015-06-15 11:27:27 +1000

    added better arguments and removed the second publications_by_type command

--- make-publications-markdown.py
+++ make-publications-markdown.py
@@ -6,7 +6,9 @@ print("Make the Reference List files")
 #call("pandoc publications.tex -o pubs.md --bibliography publications.bib")
 pandoc = "/usr/local/bin/pandoc "
 input_file = " publications.tex "
+type_input_file = " publications_by_type.md "
 arguments = " --bibliography publications.bib --csl acm-sigchi-proceedings.csl "
+inline_args = " --bibliography publications.bib --csl sigchi-inline.csl "
 os.system(pandoc + input_file +  " -o publications.html " + arguments)
-os.system("pandoc --bibliography publications.bib --csl sigchi-inline.csl -o publications_by_type.html -s publications_by_type.md")
-os.system("pandoc --bibliography publications.bib --csl sigchi-inline.csl --template html.template -o publications_by_type2.html -s publications_by_type.md")
+os.system(pandoc + inline_args + " -o publications_by_type.html -s " + type_input_file)
+#os.system(pandoc + inline_args + " --template html.template -o publications_by_type2.html -s " + type_input_file)
