5865ceb1fb8478ffb6cef4f34a81c08b6cd708c2
Charles Martin <cpm@charlesmartin.com.au>
2018-02-21 17:47:04 +0100

    back to old naming scheme

--- docs/test-locally.sh
+++ docs/test-locally.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cp ../references.bib publications/references.bib
-cp ../references.bib _bibliography/references.bib
+cp ../publications.bib publications/references.bib
+cp ../publications.bib _bibliography/references.bib
 bundle exec jekyll serve
