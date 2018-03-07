327a632f5ef96b1a6fc00a1f6654d47b56a05948
Charles Martin <cpm@charlesmartin.com.au>
2018-03-07 10:56:36 +0100

    update test script

--- docs/test-locally.sh
+++ docs/test-locally.sh
@@ -4,7 +4,7 @@ bib2bib -ob ./publications/journalarticles.bib -c 'keywords : ".*journal-article
 bib2bib -ob ./publications/refereedconferencepapers.bib -c 'keywords : ".*conference-paper.*" and keywords : ".*refereed.*" and not keywords : ".*non-refereed.*"' ../publications.bib 
 bib2bib -ob ./publications/underreview.bib -c 'keywords : ".*under-review.*"' ../publications.bib 
 bib2bib -ob ./publications/nonrefereedconferencepapers.bib -c 'keywords : ".*conference-paper.*" and keywords : ".*non-refereed.*"' ../publications.bib 
-bib2bib -ob ./publications/nonrefereedconferencepresentations.bib -c 'keywords : ".*conference-presentation.*" and keywords : ".*refereed.*" and not keywords : ".*non-refereed.*"' ../publications.bib 
+bib2bib -ob ./publications/nonrefereedconferencepresentations.bib -c 'keywords : ".*conference-presentation.*" and keywords : ".*non-refereed.*"' ../publications.bib 
 bib2bib -ob ./publications/opendata.bib -c 'keywords : ".*open-data.*"' ../publications.bib 
 bib2bib -ob ./publications/opensourceproject.bib -c 'keywords : ".*open-source-code-project.*"' ../publications.bib 
 bib2bib -ob ./publications/artisticperformance.bib -c 'keywords : ".*artistic-performance.*"' ../publications.bib 
