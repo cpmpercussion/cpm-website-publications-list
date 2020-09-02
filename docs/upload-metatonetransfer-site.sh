3e8fafcd41574e203bcacfd03008bc48e6fc11b9
Charles Martin <cpm@charlesmartin.com.au>
2020-09-02 23:20:41 +1000

    fixed publish script

--- docs/upload-metatonetransfer-site.sh
+++ docs/upload-metatonetransfer-site.sh
@@ -2,4 +2,6 @@
 JEKYLL_ENV=production bundle exec jekyll build
 chmod -R a+rX _site
 # Setup permissions: chmod -R a+rX ~/www_docs
-scp -P 21098 -r _site/* metawezn@198.187.29.146:public_html/
+# scp -P 21098 -r _site/* metawezn@198.187.29.146:public_html/
+rsync -rvz -e 'ssh -p 21098' --progress _site/* metawezn@198.187.29.146:public_html/
+
