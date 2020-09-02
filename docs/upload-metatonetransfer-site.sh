42f4d1ee63ed1d61837243ab1eae07017a709ad3
Charles Martin <cpm@charlesmartin.com.au>
2020-09-02 21:45:57 +1000

    new upload script

--- docs/upload-metatonetransfer-site.sh
+++ docs/upload-metatonetransfer-site.sh
@@ -2,4 +2,4 @@
 JEKYLL_ENV=production bundle exec jekyll build
 chmod -R a+rX _site
 # Setup permissions: chmod -R a+rX ~/www_docs
-scp -r _site/* metawezn@198.187.29.146:public_html/ -P 21098
+scp -P 21098 -r _site/* metawezn@198.187.29.146:public_html/
